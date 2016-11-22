92ca6d4abe864833887257a70032a8ac3634f428
Fayadh A <fayadh56@gmail.com>
2016-11-22 00:23:34 -0500

    updated main to include a small menu

--- main.cpp
+++ main.cpp
@@ -9,9 +9,25 @@ using namespace std;
 
 int main()
 {
-	
-	TopSpin a(20, 4);
 
+	int gameBoard, spinSize;
+	
+	cout << "Welcome to TopSpin - A game created by Fayadh A. \n";
+	cout << "This game is base on the popular TopSpin Puzzle \n";
+
+	cout << "Please enter the Game Board Size \n";
+	cin >> gameBoard;
+
+	cout << "Please enter the Spinner Size you'd like \n";
+	cin >> spinSize;
+
+
+	// Creating game board
+	// TEST
+	TopSpin theGameBoard(gameBoard, spinSize);
+
+	// cout << a "\n"; ???
+	// THIS IS FOR TESTING. 
 	
 
 	
